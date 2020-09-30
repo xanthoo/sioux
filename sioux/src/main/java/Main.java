2ef8cfd7b43613addf3db9e31edec145831a66e1
Radoslav <45752950+xanthoo@users.noreply.github.com>
2020-09-30 12:30:55 +0200

    Added gitignore, added visitor GUI

---
+++ sioux/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    private JTabbedPane tabbedPane1;
+    private JTextField nameTextField;
+    private JTextField licensePlateTextField;
+    private JTextField notesTextField;
+    private JButton saveButton;
+    private JButton cancelButton;
+    private JTextArea upcommingAppoinmentsTextArea;
+}
