d59b9e14cdd666ff200dfb094043e4241cef6502
Jelle Stukstette <j.stukstette@student.fontys.nl>
2020-10-07 17:02:22 +0200

    Added edit and search functions.

--- src/Sioux/appointment/AppointmentController.java
+++ src/Sioux/appointment/AppointmentController.java
@@ -26,4 +26,47 @@ public class AppointmentController {
         }
         return null;
     }
+
+    public void editEvent(Event event){
+     for (Event e : eventList){
+         if (e.getId() == event.getId()){
+             e.setStart(event.getStart());
+             e.setEnd(event.getEnd());
+             e.setSubject(event.getSubject());
+             e.setVisitor(event.getVisitor());
+             eventList.set(eventList.indexOf(e), e);
+         }
+     }
+    }
+
+    public List<Event> searchForEventString(String searchTerm){
+        List<Event> filteredList= new ArrayList<>();
+        for (Event e : eventList)
+            if (e.getSubject().contains(searchTerm) || e.getVisitor().contains(searchTerm)){
+                filteredList.add(e);
+            }
+        return filteredList;
+    }
+
+    public List<Event> searchForEventDate(LocalDate searchDate){
+        List<Event> filteredList= new ArrayList<>();
+        for (Event e : eventList){
+            if (e.getStart().equals(searchDate)){
+                filteredList.add(e);
+            }
+        }
+        return filteredList;
+    }
+
+    public List<Event> searchEventStringDate(String term, LocalDate searchDate) {
+        List<Event> filteredList = new ArrayList<>();
+        for (Event e : eventList){
+            if (e.getSubject().contains(term) || e.getVisitor().contains(term)){
+                if (e.getStart().equals(searchDate)){
+                    filteredList.add(e);
+                }
+            }
+        }
+        return filteredList;
+    }
 }
